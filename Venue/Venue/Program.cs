247ca0104d7714ba367b8a7a5a47b55aee2ff08d
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-12-05 18:48:03 -0500

    Venue initial commit & complete

---
+++ Venue/Venue/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Venue
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the name of your venue?");
+            string venue = args[0];
+            Console.WriteLine(Announce(venue));
+        }
+
+        static string Announce(string venue)
+        {
+            return venue + " will have bands performing tonight.";
+        }
+    }
+}
+
