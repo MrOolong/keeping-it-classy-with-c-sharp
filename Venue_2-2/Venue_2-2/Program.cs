3916055f62008ff983e526f0774385e5059f2981
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-12-05 19:14:00 -0500

    venue 2.2 lesson

---
+++ Venue_2-2/Venue_2-2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Venue_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Venue
+    {
+        string Name;
+
+        void Announce()
+        {
+            Console.WriteLine(Name + " will have several bands playing tonight!");
+        }
+    }
+
+}
