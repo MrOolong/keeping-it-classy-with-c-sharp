07094ef7bc8ade0de0e5ab11b26b40b4194b6ead
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-12-06 21:41:49 -0500

    using foreach loops to print the musicians

---
+++ ForeachLoops4-3/ForeachLoops4-3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForeachLoops4_3
+{
+    class Program
+    {
+        public string Name;
+        public List<Band> Bands = new List<Band>();
+
+        public void AddBand(string name)
+        {
+            Band band = new Band();
+            band.Name = name;
+            Bands.Add(band);
+        }
+
+        public void Announce()
+        {
+            Console.WriteLine(Name + " will have " + Bands.Count + " bands playing tonight!");
+            foreach (var band in Bands)
+            {
+                band.Announce();
+            }
+
+        }
+    }
+}
